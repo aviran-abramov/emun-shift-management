c5373522e5ec102672332e4d0f6ca1781a5cd7ff
Aviran <aviranabramov@gmail.com>
2026-04-06 14:30:29 +0300

    feat(auth): create ui-only sign-in page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SignInPage() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <div className="w-full md:max-w-lg flex px-4 flex-col items-center gap-16 mb-16 md:mb-20 md:gap-20">
+        <Image
+          src="/emun-logo.png"
+          alt="Emun logo"
+          height={70}
+          width={206}
+        />
+
+        <Card className="w-full max-w-sm shadow-md">
+          <CardHeader className="flex flex-col items-center">
+            <h2 className="text-2xl font-bold">התחברות</h2>
+            <CardDescription>מערכת לניהול משמרות</CardDescription>
+          </CardHeader>
+
+          <CardContent className="flex flex-col gap-3">
+            <form className="flex flex-col gap-2">
+              <Field>
+                <FieldLabel className="px-1">שם משתמש / אימייל</FieldLabel>
+                <Input placeholder="יוסי כהן" />
+              </Field>
+
+              <Field>
+                <div className="flex items-center justify-between px-1">
+                  <FieldLabel>סיסמה</FieldLabel>
+                  {/* TODO: Create forgot-password page */}
+                  <Link href="/forgot-password" className="text-blue-500 underline">שכחתי סיסמה</Link>
+                </div>
+                <Input placeholder="123456" />
+              </Field>
+            </form>
+          </CardContent>
+
+          <CardFooter className="flex flex-col items-center">
+            <Button className="self-stretch">התחבר</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </main >
+  )
+}
