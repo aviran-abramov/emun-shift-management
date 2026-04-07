88bdd1bceb955d3226d292371a374fa814191bc1
Aviran Abramov <aviranabramov@gmail.com>
2026-04-07 15:11:10 +0300

    feat(admin): add page for employees with table

---
+++ app/admin/employees/columns.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import {Badge} from "@/components/ui/badge";
+import {ColumnDef} from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Employee = {
+    id: string;
+    isActive: boolean;
+    firstName: string;
+    lastName: string;
+    username: string;
+    createdAt: Date;
+};
+
+export const columns: ColumnDef<Employee>[] = [
+    {
+        accessorKey: "isActive",
+        header: "סטטוס",
+        cell: ({row}) =>
+            row.getValue("isActive") ? (
+                <Badge className="bg-green-50 text-green-700 dark:bg-green-950 dark:text-green-300">
+                    פעיל
+                </Badge>
+            ) : (
+                <Badge variant="secondary">מושבת</Badge>
+            ),
+    },
+    {
+        accessorKey: "firstName",
+        header: "שם פרטי",
+    },
+    {
+        accessorKey: "lastName",
+        header: "שם משפחה",
+    },
+    {
+        accessorKey: "username",
+        header: "שם משתמש",
+    },
+    {
+        accessorKey: "createdAt",
+        header: "תאריך הוספה",
+        cell: ({row}) =>
+            new Date(row.getValue("createdAt")).toLocaleDateString("en-GB", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+            }),
+    },
+];
