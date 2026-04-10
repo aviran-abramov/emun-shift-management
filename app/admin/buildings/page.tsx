22d0a50b5fe14e92d17c049f19eb171b43dddaec
Aviran Abramov <aviranabramov@gmail.com>
2026-04-10 10:13:12 +0300

    feat(admin): create buildings page

---
+++ app/admin/buildings/page.tsx
@@ -0,0 +1,64 @@
+import {Metadata} from "next";
+import {columns} from "./columns";
+import {DataTable} from "./data-table";
+import prisma from "@/lib/prisma";
+import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
+import {Field, FieldLabel} from "@/components/ui/field";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Building} from "@/app/generated/prisma/client";
+
+export const metadata: Metadata = {
+    title: "בניינים",
+};
+
+async function getBuildings(): Promise<Building[]> {
+    return await prisma.building.findMany();
+}
+
+export default async function BuildingsPage() {
+    const buildings = await getBuildings();
+
+    return (
+        <div className="p-6 flex flex-col gap-8">
+            <div>
+                <h2 className="text-2xl font-bold mb-2 mr-2">
+                    רשימת בניינים ({buildings.length})
+                </h2>
+                <DataTable columns={columns} data={buildings} />
+            </div>
+            <Card className="w-full max-w-sm shadow-md">
+                <CardHeader className="flex flex-col items-center">
+                    <h2 className="text-2xl font-bold">הוסף בניין</h2>
+                </CardHeader>
+
+                <CardContent className="flex flex-col gap-3">
+                    <form className="flex flex-col gap-2">
+                        <Field>
+                            <FieldLabel className="px-1">שם הבניין</FieldLabel>
+                            <Input />
+                        </Field>
+                        <Field>
+                            <FieldLabel className="px-1">רחוב</FieldLabel>
+                            <Input />
+                        </Field>
+                        <Field>
+                            <FieldLabel className="px-1">עיר</FieldLabel>
+                            <Input />
+                        </Field>
+                    </form>
+                </CardContent>
+
+                <CardFooter className="flex flex-col items-center gap-2">
+                    <Button className="self-stretch">הוסף</Button>
+                    <Button
+                        variant={"outline"}
+                        className="self-stretch border-primary"
+                    >
+                        איפוס טופס
+                    </Button>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+}
