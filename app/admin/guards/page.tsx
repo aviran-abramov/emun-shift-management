9a60cd4b90588232869f371992499f782ad0b1ec
Aviran Abramov <aviranabramov@gmail.com>
2026-04-11 09:51:18 +0300

    refactor(admin): use semantic sections

--- app/admin/guards/page.tsx
+++ app/admin/guards/page.tsx
@@ -29,46 +29,55 @@ export default async function GuardsPage() {
 
     return (
         <PageContainer>
-            <div>
+            <section>
                 <PageTitle title="רשימת שומרים" count={guards.length} />
                 <DataTable columns={columns} data={guards} />
-            </div>
-            <Card className="w-full max-w-sm shadow-md">
-                <CardHeader className="flex flex-col items-center">
-                    <h2 className="text-2xl font-bold">הוסף שומר</h2>
-                </CardHeader>
+            </section>
 
-                <CardContent className="flex flex-col gap-3">
-                    <form className="flex flex-col gap-2">
-                        <Field>
-                            <FieldLabel className="px-1">שם פרטי</FieldLabel>
-                            <Input />
-                        </Field>
-                        <Field>
-                            <FieldLabel className="px-1">שם משפחה</FieldLabel>
-                            <Input />
-                        </Field>
-                        <Field>
-                            <FieldLabel className="px-1">שם משתמש</FieldLabel>
-                            <Input />
-                        </Field>
-                        <Field>
-                            <FieldLabel className="px-1">סיסמה</FieldLabel>
-                            <Input />
-                        </Field>
-                    </form>
-                </CardContent>
+            <section>
+                <Card className="w-full max-w-sm shadow-md">
+                    <CardHeader className="flex flex-col items-center">
+                        <h2 className="text-2xl font-bold">הוסף שומר</h2>
+                    </CardHeader>
 
-                <CardFooter className="flex flex-col items-center gap-2">
-                    <Button className="self-stretch">הוסף</Button>
-                    <Button
-                        variant={"outline"}
-                        className="self-stretch border-primary"
-                    >
-                        איפוס טופס
-                    </Button>
-                </CardFooter>
-            </Card>
+                    <CardContent className="flex flex-col gap-3">
+                        <form className="flex flex-col gap-2">
+                            <Field>
+                                <FieldLabel className="px-1">
+                                    שם פרטי
+                                </FieldLabel>
+                                <Input />
+                            </Field>
+                            <Field>
+                                <FieldLabel className="px-1">
+                                    שם משפחה
+                                </FieldLabel>
+                                <Input />
+                            </Field>
+                            <Field>
+                                <FieldLabel className="px-1">
+                                    שם משתמש
+                                </FieldLabel>
+                                <Input />
+                            </Field>
+                            <Field>
+                                <FieldLabel className="px-1">סיסמה</FieldLabel>
+                                <Input />
+                            </Field>
+                        </form>
+                    </CardContent>
+
+                    <CardFooter className="flex flex-col items-center gap-2">
+                        <Button className="self-stretch">הוסף</Button>
+                        <Button
+                            variant={"outline"}
+                            className="self-stretch border-primary"
+                        >
+                            איפוס טופס
+                        </Button>
+                    </CardFooter>
+                </Card>
+            </section>
         </PageContainer>
     );
 }
