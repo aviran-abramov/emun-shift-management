ce2f15b52df3cbdb67c239a4d09fbd1ea26f6951
Aviran Abramov <aviranabramov@gmail.com>
2026-04-11 19:02:15 +0300

    style(layout): add items-start to PageContainer

--- components/layout/PageContainer.tsx
+++ components/layout/PageContainer.tsx
@@ -1,3 +1,5 @@
 export function PageContainer({children}: {children: React.ReactNode}) {
-    return <div className="p-6 flex flex-col gap-8">{children}</div>;
+    return (
+        <div className="p-6 flex flex-col items-start gap-8">{children}</div>
+    );
 }
