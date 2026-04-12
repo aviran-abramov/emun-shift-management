a14af09cbeff364de72a2a484ca29741345fe906
Aviran Abramov <aviranabramov@gmail.com>
2026-04-12 10:42:16 +0300

    fix(sidebar): add type to toggle button

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -58,6 +58,7 @@ function ExpandedSidebar({userType, navItems, onToggle}: ExpandedSidebarProps) {
                 </Link>
 
                 <button
+                    type="button"
                     onClick={onToggle}
                     className="p-2 hover:bg-gray-300 rounded-md"
                 >
