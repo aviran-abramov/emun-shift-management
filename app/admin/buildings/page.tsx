94f504824ef8e3585a00c05d8af133c589b36248
Aviran Abramov <aviranabramov@gmail.com>
2026-04-13 09:45:25 +0300

    refactor(buildings): extract CreateBuildingForm

--- app/admin/buildings/page.tsx
+++ app/admin/buildings/page.tsx
@@ -1,14 +1,12 @@
 import {Metadata} from "next";
 import {columns} from "./columns";
 import prisma from "@/lib/prisma";
-import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
-import {Field, FieldLabel} from "@/components/ui/field";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
+import {Card, CardHeader} from "@/components/ui/card";
 import {Building} from "@/app/generated/prisma/client";
 import {DataTable} from "@/components/ui/data-table";
 import {PageContainer} from "@/components/layout/page-container";
 import {PageTitle} from "@/components/layout/page-title";
+import {CreateBuildingForm} from "@/components/buildings/create-form";
 
 export const metadata: Metadata = {title: "בניינים"};
 
@@ -32,43 +30,7 @@ export default async function BuildingsPage() {
                         <h2 className="text-2xl font-bold">הוסף בניין</h2>
                     </CardHeader>
 
-                    <form className="flex flex-col gap-4">
-                        <CardContent className="flex flex-col">
-                            <Field>
-                                <FieldLabel htmlFor="name" className="px-1">
-                                    שם הבניין
-                                </FieldLabel>
-                                <Input type="text" id="name" />
-                            </Field>
-
-                            <Field>
-                                <FieldLabel htmlFor="street" className="px-1">
-                                    רחוב
-                                </FieldLabel>
-                                <Input type="text" id="street" />
-                            </Field>
-
-                            <Field>
-                                <FieldLabel htmlFor="city" className="px-1">
-                                    עיר
-                                </FieldLabel>
-                                <Input type="text" id="city" />
-                            </Field>
-                        </CardContent>
-
-                        <CardFooter className="flex flex-col items-center gap-2">
-                            <Button type="submit" className="self-stretch">
-                                הוסף
-                            </Button>
-                            <Button
-                                type="reset"
-                                variant={"outline"}
-                                className="self-stretch border-primary"
-                            >
-                                איפוס טופס
-                            </Button>
-                        </CardFooter>
-                    </form>
+                    <CreateBuildingForm />
                 </Card>
             </section>
         </PageContainer>
