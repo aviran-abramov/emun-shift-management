65a06e384318b68b158ff639fd798701fb4fdbc7
Aviran Abramov <aviranabramov@gmail.com>
2026-04-13 14:35:58 +0300

    refactor(buildings): extract CreateBuildingDialog

---
+++ components/buildings/create-building-dialog.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { CreateBuildingForm } from "@/components/buildings/create-form";
+import { Button } from "@/components/ui/button";
+import {
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  Dialog,
+} from "@/components/ui/dialog";
+
+export function CreateBuildingDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="self-start">
+          <span className="text-xl">+</span>
+          <span>הוסף בניין</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>הוסף בניין</DialogTitle>
+          <DialogDescription>מלא את פרטי הבניין החדש</DialogDescription>
+        </DialogHeader>
+
+        <CreateBuildingForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
