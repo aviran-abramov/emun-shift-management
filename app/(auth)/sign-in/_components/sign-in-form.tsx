665f77339038dd7a4973d7596c557fc984354f99
Aviran Abramov <aviranabramov@gmail.com>
2026-04-20 14:26:12 +0300

    feat(auth): add zod validation to sign-in form

--- app/(auth)/sign-in/_components/sign-in-form.tsx
+++ app/(auth)/sign-in/_components/sign-in-form.tsx
@@ -2,31 +2,75 @@
 
 import { Button } from "@/components/ui/button";
 import { CardContent, CardFooter } from "@/components/ui/card";
-import { Field, FieldLabel } from "@/components/ui/field";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { Link } from "lucide-react";
+import Link from "next/link";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignInFormData, SignInSchema } from "@/lib/validators/auth";
 
 export default function SignInForm() {
+  const form = useForm<SignInFormData>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: SignInFormData) => {};
+
   return (
-    <form className="flex flex-col gap-4">
+    <form
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="flex flex-col gap-4"
+    >
       <CardContent className="flex flex-col gap-2">
-        <Field>
-          <FieldLabel htmlFor="identifier" className="px-1">
-            שם משתמש / אימייל
-          </FieldLabel>
-          <Input type="text" id="identifier" placeholder="yossicoh123" />
-        </Field>
+        <Controller
+          name="username"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor={field.name} className="px-1">
+                שם משתמש
+              </FieldLabel>
+              <Input
+                {...field}
+                type="text"
+                id={field.name}
+                placeholder="yossicoh123"
+                aria-invalid={fieldState.invalid}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
 
-        <Field>
-          <div className="flex items-center justify-between px-1">
-            <FieldLabel htmlFor="password">סיסמה</FieldLabel>
-
-            <Link href="/forgot-password" className="text-blue-500 underline">
-              שכחתי סיסמה
-            </Link>
-          </div>
-          <Input type="password" id="password" placeholder="123456" />
-        </Field>
+        <Controller
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <div className="flex items-center justify-between px-1">
+                <FieldLabel htmlFor={field.name}>סיסמה</FieldLabel>
+                <Link
+                  href="/forgot-password"
+                  className="text-blue-500 underline"
+                >
+                  שכחתי סיסמה
+                </Link>
+              </div>
+              <Input
+                {...field}
+                type="password"
+                id={field.name}
+                placeholder="123456"
+                aria-invalid={fieldState.invalid}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
       </CardContent>
 
       <CardFooter className="flex flex-col items-center">
