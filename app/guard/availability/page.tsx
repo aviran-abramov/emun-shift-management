cd72d86a56b43dfb9ff0153c69456847eb95e5b5
Aviran Abramov <aviranabramov@gmail.com>
2026-04-20 11:11:27 +0300

    feat(guard): add ui for availability page

--- app/guard/availability/page.tsx
+++ app/guard/availability/page.tsx
@@ -1,3 +1,88 @@
+import { PageContainer } from "@/components/layout/page-container";
+import { PageTitle } from "@/components/layout/page-title";
+import { Button } from "@/components/ui/button";
+import { Field, FieldLabel, FieldGroup } from "@/components/ui/field";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function GuardAvailabilityPage() {
-  return <div>הגשת משמרות</div>;
+  const availability = [];
+
+  return (
+    <PageContainer>
+      <div className="flex flex-col gap-4">
+        <PageTitle title="הגשת משמרות" />
+
+        <section className="flex flex-col gap-2 rounded-lg border p-4 shadow-sm">
+          <h2 className="text-lg font-bold">הוסף משמרת</h2>
+          <form>
+            <FieldGroup>
+              <Field className="max-w-28">
+                <FieldLabel>יום</FieldLabel>
+                <Select dir="rtl">
+                  <SelectTrigger>
+                    <SelectValue placeholder="בחר" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="ראשון">ראשון</SelectItem>
+                      <SelectItem value="שני">שני</SelectItem>
+                      <SelectItem value="שלישי">שלישי</SelectItem>
+                      <SelectItem value="רביעי">רביעי</SelectItem>
+                      <SelectItem value="חמישי">חמישי</SelectItem>
+                      <SelectItem value="שישי">שישי</SelectItem>
+                      <SelectItem value="שבת">שבת</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </Field>
+
+              <Field className="max-w-28">
+                <FieldLabel>משמרת</FieldLabel>
+                <Select dir="rtl">
+                  <SelectTrigger>
+                    <SelectValue placeholder="בחר" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="MORNING">בוקר</SelectItem>
+                      <SelectItem value="EVENING">ערב</SelectItem>
+                      <SelectItem value="NIGHT">לילה</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </Field>
+
+              <Field>
+                <FieldLabel>הערות למשמרת</FieldLabel>
+                <Textarea id="notes" />
+              </Field>
+
+              <Button className="self-baseline">הוסף משמרת</Button>
+            </FieldGroup>
+          </form>
+        </section>
+
+        <section className="rounded-lg border p-4 shadow-sm">
+          <h2 className="text-lg font-bold">המשמרות שהגשתי</h2>
+          {availability.length !== 0 ? (
+            <ul>
+              <li>ראשון בוקר</li>
+            </ul>
+          ) : (
+            <p className="text-muted-foreground">
+              לא הוגשו עדיין משמרות לשבוע הבא
+            </p>
+          )}
+        </section>
+      </div>
+    </PageContainer>
+  );
 }
