c9218f7bfb9aa9185c6db52393acc912c92cf2c0
Aviran Abramov <aviranabramov@gmail.com>
2026-04-24 14:08:50 +0300

    perf(logo): use eager loading

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,7 +8,13 @@ export default function AuthLayout({
   return (
     <main className="min-h-screen flex flex-col justify-center items-center bg-radial from-[#FFAB4F] to-[#D95E00]">
       <div className="w-full md:max-w-lg flex px-4 flex-col items-center gap-16 mb-16 md:mb-20 md:gap-20">
-        <Image src="/emun-logo.png" alt="Emun logo" height={70} width={206} />
+        <Image
+          src="/emun-logo.png"
+          alt="Emun logo"
+          height={70}
+          width={206}
+          loading="eager"
+        />
 
         {children}
       </div>
