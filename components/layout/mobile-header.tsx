1a8e93b32c0d0e0a46900650f1e43533a168af20
Aviran Abramov <aviranabramov@gmail.com>
2026-04-24 06:54:19 +0300

    fix(mobile-header): remove homeHref prop

--- components/layout/mobile-header.tsx
+++ components/layout/mobile-header.tsx
@@ -1,17 +1,14 @@
 import { MenuTrigger } from "@/components/layout/menu-trigger";
 import Image from "next/image";
-import Link from "next/link";
 
-export function MobileHeader({ homeHref }: { homeHref: string }) {
+export function MobileHeader() {
   return (
     <div className="relative flex items-center justify-between md:hidden border-b bg-primary px-2 py-1">
       <MenuTrigger />
       <span className="absolute left-1/2 -translate-x-1/2 font-semibold">
         מוקד אמון
       </span>
-      <Link href={homeHref}>
-        <Image src="/favicon.ico" height={30} width={30} alt="emun logo" />
-      </Link>
+      <Image src="/favicon.ico" height={30} width={30} alt="emun logo" />
     </div>
   );
 }
