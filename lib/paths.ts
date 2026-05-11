997ceb884fea7562597fa0920bf2f1a2cbd3fa33
Aviran Abramov <aviranabramov@gmail.com>
2026-05-11 06:55:10 +0300

    chore(admin): change default path to schedule

--- lib/paths.ts
+++ lib/paths.ts
@@ -2,5 +2,5 @@ import type { Role } from "@/app/generated/prisma/enums";
 
 export const DEFAULT_PATHS: Record<Role, string> = {
   GUARD: "/guard/availability",
-  MANAGER: "/admin/guards",
+  MANAGER: "/admin/schedule/1",
 };
