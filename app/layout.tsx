70c9bc7fb93dc1ec8fa807118c3e437c36ef1de6
Aviran Abramov <aviranabramov@gmail.com>
2026-05-14 14:53:44 +0300

    feat(ui): set up sonner toasts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_Hebrew, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const fontSans = Noto_Sans_Hebrew({
   subsets: ["hebrew"],
@@ -43,7 +44,10 @@ export default function RootLayout({
         fontSans.variable,
       )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
